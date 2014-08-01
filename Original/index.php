135a35fc9b7d5eeb6ac693697635b8826bce0540
Beth Crane <bethc@cse.unsw.edu.au>
2014-08-01 18:27:44 +1000

    Removed some old files

--- Original/index.php
+++
@@ -1,8 +0,0 @@
-<html>
-  <head>
-    <title>Coming Soon...</title>
-  </head>
-  <body leftmargin="0" topmargin="0">
-    <iframe src ="http://servers.syrahost.com/?action=coming_soon&server_id=32" width="100%" height=100% frameborder=0> </iframe>
-  </BODY>
-</HTML>
